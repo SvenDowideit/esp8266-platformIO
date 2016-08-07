d1778e6d27c41c252df1121b718a1b29dc946784
Sven Dowideit <SvenDowideit@home.org.au>
2016-08-06 22:56:59 -0700

    And now OLED displays the auto-logo
    Signed-off-by: Sven Dowideit <SvenDowideit@home.org.au>

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,34 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 
+// OLED example from D1_mini_examples
+#include <SPI.h>
+#include <Wire.h>  // Include Wire if you're using I2C
+#include <SFE_MicroOLED.h>  // Include the SFE_MicroOLED library
+
+#define PIN_RESET 255  //
+#define DC_JUMPER 0  // I2C Addres: 0 - 0x3C, 1 - 0x3D
+
+MicroOLED oled(PIN_RESET, DC_JUMPER); // Example I2C declaration
+// I2C is great, but will result in a much slower update rate. The
+// slower framerate may be a worthwhile tradeoff, if you need more
+// pins, though.
+void setupOLED()
+{
+  // These three lines of code are all you need to initialize the
+  // OLED and print the splash screen.
+
+  // Before you can start using the OLED, call begin() to init
+  // all of the pins and configure the OLED.
+  oled.begin();
+  // clear(ALL) will clear out the OLED's graphic memory.
+  // clear(PAGE) will clear the Arduino's display buffer.
+  oled.clear(ALL);  // Clear the display's memory (gets rid of artifacts)
+  // To actually draw anything on the display, you must call the
+  // display() function.
+  oled.display();
+}
+
 // Wemost example http://www.esp8266learning.com/wemos-mini-ws2812b-example.php
 #include <Adafruit_NeoPixel.h>
 #define PIN D2
@@ -120,6 +148,8 @@ void setup(void){
   server.begin();
   Serial.println("HTTP server started");
   setWhite();
+
+  setupOLED();
 }
 
 void loop(void){
